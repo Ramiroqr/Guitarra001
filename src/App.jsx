5dc4ba5339fe5bd35770e95dbcc788249d4ffb2d
Ramiroqr <ramiroquisper@gmail.com>
2024-06-02 08:23:37 -0500

    build product

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,13 @@ import { db } from "./data/db"
 
 function App() {
 
-  const [data, setData] = useState(db)
-  const [cart, setCart] = useState([])
+  const initialCart = () => {
+    const localStorageCart = localStorage.getItem('cart')
+    return localStorageCart ? JSON.parse(localStorageCart) : []
+  }
+
+  const [data] = useState(db)
+  const [cart, setCart] = useState(initialCart)
 
   function addToCart(item) {
     
